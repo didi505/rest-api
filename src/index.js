230da8e90665ea87b26710ec2f36b402badc7a9d
Dilara Özsoy <dilara.oezsoy@student.hs-rm.de>
2026-07-05 15:49:39 +0200

    feat: Task-Modell und CRUD-Routen

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const tasksRouter = require('./routes/tasks');
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.json({ name: 'Tasks-API', version: '1.0.0' });
+});
+
+app.use('/api/tasks', tasksRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server läuft auf http://localhost:${PORT}`);
+});
